c9e763a119fe0bd07e9349e85e953df69221b737
Thomas Desrosiers <thomas@databricks.com>
2017-05-07 18:00:41 -0700

    Add initial fuzzing function

---
+++ fuzz_testing/fuzz.go
@@ -0,0 +1,39 @@
+package goavro
+
+import (
+	"bytes"
+
+	"github.com/karrick/goavro"
+)
+
+func init() {
+	goavro.MaxAllocationSize = 100 * 1024 * 1024
+}
+
+func Fuzz(data []byte) int {
+	ocfr, err := goavro.NewOCFReader(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+
+	var datums []interface{}
+	for ocfr.Scan() {
+		datum, err := ocfr.Read()
+		if err != nil {
+			return 0
+		}
+		datums = append(datums, datum)
+	}
+
+	b := new(bytes.Buffer)
+	ocfw, err := goavro.NewOCFWriter(
+		goavro.OCFWriterConfig{
+			W:      b,
+			Schema: ocfr.Schema(),
+		})
+	if err := ocfw.Append(datums); err != nil {
+		panic(err)
+	}
+
+	return 1
+}
